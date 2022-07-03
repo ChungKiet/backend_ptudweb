51aa12b667462982b2b054b509cac6f26cea70b4
Nash-equilbrm <dohoainam17052001@gmail.com>
2022-07-03 22:16:52 +0700

    get all user

--- routes/manager.js
+++ routes/manager.js
@@ -3,7 +3,10 @@ const route = express.Router();
 
 const ManagerController = require('../controllers/manager_controller')
 
-route.get('/add-user', ManagerController.add_user);;
+route.get('/add-user', ManagerController.add_user);
+
+route.get('/get-all-user', ManagerController.get_all_user);
+
 
 route.delete('/delete-user', ManagerController.delete_user);
 
