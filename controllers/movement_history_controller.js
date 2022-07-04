5cc509fcaf497c57ca9d0a83c2793b1ca1a2167a
ChungKiet <chtkkiet@gmail.com>
2022-07-04 17:04:55 +0700

    fix-add-mov-his

--- controllers/movement_history_controller.js
+++ controllers/movement_history_controller.js
@@ -7,12 +7,23 @@ class MovHisController {
       // console.log({ name, gender, birthday, email, username, password });
       const movHis = await MovementHis.findOne({ id_user: id_user});
       if (movHis) {
+            const movHis = await MovementHis.updateOne({id_user: id_user}, {$push: {info: {
+               id_other: info.id_other,
+               address: info.address,
+               date: info.date
+            }}})
             res.send({
-               "msg": 3, 'mov_his': null
+               "msg": 3, 'mov_his': movHis
             });
          }
       try {
-         const movHis = await MovementHis.create({id_user, info});
+         const movHis = await MovementHis.create({id_user: id_user, info : [
+            {
+               id_other: info.id_other,
+               address: info.address,
+               date: info.date
+            }
+         ]});
          res.send({ "msg": 1, 'mov_his': movHis });
       }
       catch (err) {
